9fc808885f7c5af29b3a19cac4a58d43e0b848eb
Sachin_Pandey <sachinpandey262523@gmail.com>
2022-09-04 14:18:13 +0530

    adding-backend

---
+++ server/database/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+
+dotenv.config();
+
+
+
+const Connection = async (username, password) => {
+    const URL = `mongodb+srv://sachin:${password}@cluster0.o4hbbxz.mongodb.net/?retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
